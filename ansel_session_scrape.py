61fd1d3674817ea75d612f2f3df12c322278a178
katrinasheehy <katrina.bell@gmail.com>
2026-02-16 18:57:03 -0800

    Update ansel_session_scrape.py

--- ansel_session_scrape.py
+++ ansel_session_scrape.py
@@ -6,9 +6,17 @@ import re
 import os
 
 def clean_and_split(val):
-if pd.isna(val) or not isinstance(val, str): return val, None
+if pd.isna(val) or not isinstance(val, str):
+return val, None
 match = re.match(r"(\d+.\d+)\s*(.*)", str(val))
-return (float(match.group(1)), match.group(2).strip()) if match else (val, None)
+if match:
+try:
+score = float(match.group(1))
+rank = match.group(2).strip()
+return score, rank
+except:
+return val, None
+return val, None
 
 def scrape_and_update():
 url = ""
