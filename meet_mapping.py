a97cb203a663940c1e71c8549b2a2458c49cab8d
katrinasheehy <katrina.bell@gmail.com>
2026-02-18 11:20:04 -0800

    Create meet_mapping.py

---
+++ meet_mapping.py
@@ -0,0 +1,9 @@
+# meet_mapping.py
+
+# MyMeetScores (MMS) IDs for 2026
+MMS_MEET_IDS = {
+    "2026 Rose Gold Classic": "93352",
+    "2026 Golden State Classic": "93026",
+    # Mardi Gras usually appears on MMS a few days after the meet
+    "2026 Mardi Gras Invitational": "" 
+}
